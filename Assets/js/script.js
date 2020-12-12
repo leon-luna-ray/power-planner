bcaa10231c8d80caa025eb327efd9531429f2fad
Ray Luna <leon.luna.ray@gmail.com>
2020-12-11 22:39:54 -0800

    Finished updating styling and got the colors to change based on the time of day. Added screenshot image to assets.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){ 
 //event listener
     $(".save-button").on("click", function(){ 
+
         //Setting up variables for the key/value pair with the entry time and the schedule entry.
         var entryTime = $(this).parent().attr("id");
         var userEntry = $(this).siblings(".block-entry").val();
@@ -9,23 +10,33 @@ $(document).ready(function(){
 
         //Save entries to local storage
         localStorage.setItem(entryTime, userEntry);
+
+        // Created variable to get entry from local storage but I wasn't able to figure out how to get it to stick.
+
+        var storedItem = localStorage.getItem(entryTime);
+
         });
 
         //append current date to top of page using moment.js
         var currentDay = moment().format("dddd MMMM Do YYYY");
         $("#currentDay").text(currentDay)
 
-        // Current hour for changing the color
-        var currentHour = moment().format("HH")
-        
-// Need a function that will change the color based on the current time. Figure out how to use "this" again to change the sibling
-
-        // function changeColor(){
-        //     if(currentHour > $(".9").attr(id).val)
-        //     $("#future").attr("id", "#past");
-        // }
-
-// changeColor();
-
+        // Function to change each block on the hour
+        $(".block-entry").each(function (){
+                var blockTime = $(this).attr("id");
+                // Converted time to 24h format
+                var currentTime = moment().format("HH")
+                // Parsed the current time and and each block time
+                var blockHour = parseInt(blockTime)
+                var currentHour = parseInt(currentTime)
+                // Else/if to change the ID for color based on time
+                if(blockHour === currentHour) {
+                        $(this).attr("id", "present")
+                } else if (blockHour < currentHour) {
+                        $(this).attr("id", "past")
+                } else if (blockHour > currentHour) {
+                        $(this).attr("id", "future")
+                }
+        });
 
 }); //jQuery wrapper
